b948e1cf372ff2e5f5f429afd16188787c40ccb9
A YLDZ <59310312+yldzali@users.noreply.github.com>
2020-05-08 12:00:32 +0100

    Update Call.java

--- src/tests/Call.java
+++ src/tests/Call.java
@@ -3,6 +3,8 @@ package tests;
 public class Call {
     public static void main(String[] args) {
         System.out.println("someone is calling.......");
+        
+        //I ll see this line on intellij
     }
 
 }
